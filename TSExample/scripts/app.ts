555d7f777c0554f1cb2f94cff15beb4ccec9b1ed
Adam Barnsley <abarnsley@gmail.com>
2018-09-27 19:05:05 +0100

    Add project files.

---
+++ TSExample/scripts/app.ts
@@ -0,0 +1,10 @@
+﻿// ES6 Modules or TypeScript
+import swal from 'sweetalert2'
+
+function helloWorld() {
+    alert('hello from alert');
+}
+
+function swalHelloWorld() {
+    swal('hello from sweet alert');
+}
---
+++ TSExample/wwwroot/build/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ES6 Modules or TypeScript
+var sweetalert2_1 = require("sweetalert2");
+function helloWorld() {
+    alert('hello from alert');
+}
+function swalHelloWorld() {
+    sweetalert2_1.default('hello from sweet alert');
+}
+//# sourceMappingURL=app.js.map
